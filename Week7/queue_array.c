a40c83af552639e2c88880f343da650511453f54
Rangu-Dimple-Bhavani <69664388+Rangu-Dimple-Bhavani@users.noreply.github.com>
2020-09-29 19:55:28 +0530

    Add files via upload

---
+++ Week7/queue_array.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define maxsize 5
+int f=-1,r=-1,queue[maxsize];
+int isfull()
+{
+      if(r==maxsize-1)
+      return 1;
+      else 
+      return 0;
+}
+int isempty()
+{
+      if(f==-1)
+      return 1;
+      else
+      return 0;
+}
+void enqueue(int data)
+{
+      if(f==-1)
+          f=0;
+      queue[++r]=data;
+}
+int dequeue()
+{
+      return(queue[f++]);
+}
+void display()
+{
+      for(int i=f;i<=r;i++)
+          printf("%d\t",queue[i]);
+}
+int main()
+{
+        int ch,x;
+        while(1)
+        {
+             printf("program of queue using array");
+             printf("\n1-enqueue\n2-dequeue\n3-display\n4-exit\n");
+             printf("enter your choice\n");
+             scanf("%d",&ch);
+             switch(ch)
+             {
+              case 1: if(isfull())
+                      {
+                        printf("queue overflow\n");
+                      }
+                     else
+                     {
+                           printf("enter data to insert:\n");
+                           scanf("%d",&x);
+                           enqueue(x);
+                     }
+                     break;
+              case 2: if(isempty())
+                     printf("queue underflow\n");
+                     else
+                     {
+                           printf("deleted element is %d \n",dequeue());
+                     }
+                     break;
+              case 3: display();
+                     break;
+              case 4: exit(0);
+           }
+       }
+}
